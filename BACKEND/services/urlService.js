fab55272888b52d42f8a6f4d9992adeaadc53ff2
Suryansh3105 <suryanshpandeywork@gmail.com>
2026-02-08 18:13:38 +0530

    feat(url): add support for custom short URL aliases

--- BACKEND/services/urlService.js
+++ BACKEND/services/urlService.js
@@ -3,7 +3,16 @@ import { generateShortUrl } from "../utils/generateShortUrl.js";
 const store = new Map();
 const MAX_RETRIES=5;
 
-export function createShortUrl(originalUrl){
+export function createShortCode(originalUrl,customAlias){
+    const normalizedAlias = customAlias?.toLowerCase();
+    if(normalizedAlias){
+        if(store.has(normalizedAlias)){
+            throw new Error(`Alias already exist`)
+        }// think about think as res i not avaible in services // service used to return data return null or throw error
+        store.set(customAlias,originalUrl);
+        return customAlias;
+    }
+
     for(let i=0;i<MAX_RETRIES;i++){
         const shortCode=generateShortUrl();
         if(!store.has(shortCode)){
