1d46bde115414a50a66f77df76c1fca1d9549fd1
Suryansh3105 <suryanshpandeywork@gmail.com>
2026-02-13 23:03:57 +0530

    added link expiration feature

--- BACKEND/controllers/url.controller.js
+++ BACKEND/controllers/url.controller.js
@@ -1,11 +1,17 @@
 import { createShortCode, getOriginalUrl } from "../services/urlService.js";
 import { shortUrlToQR } from "../services/qrService.js";
+import { dateValidation } from "../middleware/validateDate.js";
 
 async function shortenUrl(req,res){
-    const {originalUrl,customAlias} =req.body;
+    const {originalUrl,customAlias,expireAble,expireAt} =req.body;
     const {generateQR}=req.query;
+
+    if (expireAble && isExpired(expireAt)) {
+    throw new Error("date should be in future");}
     
-    const shortCode= await createShortCode(originalUrl,customAlias);
+    const newExpireAt = new Date(expireAt)
+
+    const shortCode= await createShortCode(originalUrl,customAlias,newExpireAt);
     const baseUrl=process.env.BASE_URL || `${req.protocol}://${req.get('host')}`;
     const shortUrl=`${baseUrl}/${shortCode}`;
     if(generateQR){
@@ -16,14 +22,20 @@ async function shortenUrl(req,res){
 }
 
 async function redirectToOriginalUrl(req,res){
-    try{const {shortID} = req.params;
-    const originalUrl = await getOriginalUrl(shortID);
-    if(!originalUrl){
-       return res.status(404).json({error:`url not found`})
+    try{
+        const {shortID} = req.params;
+        const originalUrl = await getOriginalUrl(shortID);
+        if(!originalUrl){
+            return res.status(404).json({error:`url not found`})
+        }
+        if(originalUrl.expireAT && Date.now() > new Date(originalUrl.expireAT).getTime()){
+        return res.status(410).json({message:`link expired`});
+        }
+
+        res.status(301).redirect(originalUrl.longUrl);
     }
-    res.status(301).redirect(originalUrl);
- }catch(err){
-     onsole.error("Redirect error:", err);
+    catch(err){
+     console.error("Redirect error:", err);
      return res.status(500).json({ error: "Server error" });
  }
 }
